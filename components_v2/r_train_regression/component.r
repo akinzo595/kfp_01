b236abd17b1509f6d70e9ddd47a0f22412e9bf24
charles adetiloye <charles@mavencode.com>
2020-07-04 17:26:39 -0500

    adding component that pulls data from google storage

---
+++ components_v2/r_train_regression/component.r
@@ -0,0 +1,23 @@
+library("optparse")
+library("RDS")
+
+regression_model <- function(training_data, loss_function, num_iterations, learning_rate, depth, output_model_path) {
+    print('hello ML training...')
+    output_model_path
+}
+option_list = list(
+  make_option(
+    "-t","--trainingdata", type="character", default=NULL, metavar="character"),
+  make_option("-l","--lossfunction", type="character", metavar="character"),
+  make_option("-n","--numIterations", type="character", metavar="character"),
+  make_option("-n","--learningRate", type="character", metavar="character"),
+  make_option("-d","--depth", type="character", metavar="character"),
+  c("-m
+  ", "--modelOutpath"), type="character", default=NULL, metavar="character")
+ 
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+output = regression_model(opt$trainingdata, opt$lossfunction, opt$numIterations, opt$learningRate, opt$depth, opt$modelOutpath )
+
+#implementation
--- readme.md
+++ readme.md
@@ -5,3 +5,8 @@
 2. conda activate kf-pipeline
 
 3. pip install -r requirements.txt
+
+
+
+#Grabing Sample Data from Chicago TaxiCab
+select 100k records -> `curl --get 'https://data.cityofchicago.org/resource/wrvz-psew.csv' --data-urlencode '$limit=100000' | tr -d '"' > taxi_data.csv`
